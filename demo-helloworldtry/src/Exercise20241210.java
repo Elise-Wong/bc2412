3b85ccc75ebbaeae48f59fd6ea1fc8b84f66438f
Elise-Wong <e.wong.hksarwork@gmail.com>
2024-12-10 12:58:24 +0800

    2412

---
+++ demo-helloworldtry/src/Exercise20241210.java
@@ -0,0 +1,29 @@
+public class Exercise20241210 {
+  public static void main(String[] args){
+    double priceForApple = 7.3;
+    double priceForOrange = 6.5;
+    int quantity0fApple = 3;
+    int quantity0fOrange = 4;
+
+    double totalAmountApple = priceForApple * quantity0fApple;
+    System.out.println(totalAmountApple);
+
+    double totalAmountOrange = priceForOrange * quantity0fOrange;
+    System.out.println(totalAmountOrange);
+
+    double totalAmount = totalAmountApple + totalAmountOrange;
+    System.out.println(totalAmount);
+
+
+
+    int mathScore = 73;
+    int englishScore = 60;
+    int historyScore = 61;
+
+    double averageScore = (mathScore + englishScore + historyScore) / 3.0;
+
+    System.out.println(averageScore);
+
+  }
+
+}
---
+++ demo-helloworldtry/src/Helloworld.java
@@ -0,0 +1,31 @@
+public class Helloworld {
+    public static void main(String[] args) {
+        int x = 3;
+        int day0fWeek = 7;
+        double y2 = 3.2;
+        x = 101;
+        x = x + 10;
+        x = x - 40;
+        x = x * 3;
+        x = x / 213;
+
+        x = x + 8 * 2 / 4 + 1;
+        x = (x + 2) * 2 / (3 + 1);
+        x = ((x + 3) / (6 + 1)) * (x + 1);
+        
+        System.out.println(x);
+
+        int x2 = 3 /2;
+        System.out.println(x2);
+
+        double x3 = 3 /2;
+        System.out.println(x3);
+
+        double x4= 3.0 / 2;
+        System.out.println(x4);
+
+        System.out.println(0 / 10);
+
+        
+    }
+}
