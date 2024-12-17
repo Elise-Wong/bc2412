52db71884de5ca111a84bc7ab5ab7e76b097ba21
Elise-Wong <e.wong.hksarwork@gmail.com>
2024-12-17 10:16:00 +0800

    20241217

--- homework/src/HW20241216Java7.java
+++ homework/src/HW20241216Java7.java
@@ -1,7 +1,13 @@
 public class HW20241216Java7 {
   public static void main(String[] args){
 
+    int[]arr = new int[]{1,2,3,4,5,6,7,8,9,10};
+
     int sum = 0;
+
+    for (int i = 0; i < arr.length; i++){
+      sum = sum+ arr[i];
+    }
 
 
     System.out.println("The sum is " + sum);
