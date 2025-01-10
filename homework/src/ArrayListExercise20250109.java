dad58973d995a6bc29f3b6566e380d47aa58da07
Elise-Wong <e.wong.hksarwork@gmail.com>
2025-01-10 11:20:25 +0800

    20250109

--- homework/src/ArrayListExercise20250109.java
+++ homework/src/ArrayListExercise20250109.java
@@ -213,7 +213,15 @@ public class ArrayListExercise20250109 {
     //System.out.println(studentSet2s.getName() + studentSet2s.getId());
 
     //09b
-    
+    String sN = "Not Found";
+    for (Student s1 : stundentSet1s){
+      for (Student s2 : stundentSet2s){
+        if (s1.getName().equals(studentSet2s)){
+          sN = s1.getName();
+          break;
+        }
+      }
+    }
 
     //09c
     studentSet1s.addAll(studentSet2s);
